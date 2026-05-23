1f63c504db260b0c4184e0ffa10800c3707c0416
michaelbranders <michael.branders@youbyme.be>
2026-05-23 11:22:05 +0200

    add charging limit as number as well

--- custom_components/ev_metron_websockets/number.py
+++ custom_components/ev_metron_websockets/number.py
@@ -25,6 +25,7 @@ async def async_setup_entry(
     async_add_entities(
         [
             ChargeDelayTimer(hub),
+            DynamicCurrentLimit(hub)
         ]
     )
 
@@ -56,4 +57,59 @@ class ChargeDelayTimer(MetronEVBaseEntity, NumberEntity):
         """Set the charge delay timer."""
         int_value = int(value)
         payload = f"TIMER{int_value}"
+        await self._hub.perform_action(payload)
+
+
+MAX_VALUE = "32"
+
+class DynamicCurrentLimit(MetronEVBaseEntity, NumberEntity):
+    """Number entity for setting the dynamic charging current limit."""
+
+    def __init__(self, hub: MetronEVHub) -> None:
+        """Initialize the number entity."""
+        super().__init__(hub)
+
+        self._attr_unique_id = f"{hub._name}_current_limit"
+        self._attr_name = f"{hub._name} current limit"
+
+        self._attr_native_min_value = 0
+        self._attr_native_max_value = MAX_VALUE
+        self._attr_native_step = 1
+
+        self._attr_native_unit_of_measurement = (
+            UnitOfElectricCurrent.AMPERE
+        )
+
+        self._attr_mode = "slider"
+        self._attr_icon = "mdi:current-ac"
+
+    @property
+    def native_value(self) -> float | None:
+        """Return the current charging current limit."""
+        try:
+            value = int(self._hub.dynamic_charging_current_limit)
+
+            if value < 0:
+                return MAX_VALUE
+
+            if value > MAX_VALUE:
+                return MAX_VALUE
+
+            return value
+
+        except (ValueError, TypeError):
+            return None
+
+    async def async_set_native_value(self, value: float) -> None:
+        """Set the charging current limit."""
+        value = int(value)
+
+        if value < 0:
+            value = MAX_VALUE
+
+        if value > MAX_VALUE:
+            value = MAX_VALUE
+
+        payload = f"SLIDE{value}"
+
         await self._hub.perform_action(payload)
